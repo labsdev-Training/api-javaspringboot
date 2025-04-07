bca1ea2c12331681989a789d37a22fab300eb29f
Rose Dias <rosedias.info@gmail.com>
2025-04-07 17:14:25 -0300

    Formatacao

--- todolist/src/main/java/br/com/api/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/api/todolist/user/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
@@ -19,19 +18,22 @@ public class UserController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel) {
+
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("Usuário Já existe");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+            System.out.println("Usuário Já existe.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+
         }
 
         var passwordHashred = BCrypt.withDefaults()
-        .hashToString(12, userModel.getPassword().toCharArray());
+                .hashToString(12, userModel.getPassword().toCharArray());
 
         userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+
     }
 }
