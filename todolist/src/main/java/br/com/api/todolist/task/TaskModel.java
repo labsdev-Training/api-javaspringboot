b9d11429feb148e2b33f974fb0c9fdd8fa45f38e
Rose Dias <rosedias.info@gmail.com>
2025-04-08 10:30:39 -0300

    Repository

---
+++ todolist/src/main/java/br/com/api/todolist/task/TaskModel.java
@@ -0,0 +1,40 @@
+package br.com.api.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * id usuário (id_usuario) descrição título data de início data de término
+ * prioridade
+ */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime stardtAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
+
+}
